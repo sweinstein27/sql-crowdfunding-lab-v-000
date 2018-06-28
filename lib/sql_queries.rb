072022155c5f929c417d644f2fd812fb38fef6d9
Scott Weinstein <sweinstein27@gmail.com>
2018-06-28 19:05:51 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT name, SUM(pledges.amount) FROM users
 JOIN pledges on users.id = pledges.user_id
 GROUP BY users.name
-ORDER BY pledges.amount, users.name"
+ORDER BY SUM(pledges.amount), users.name"
 end
 
 
