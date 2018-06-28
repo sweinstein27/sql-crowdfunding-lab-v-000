851a83ea08159ebf3566f81913e5c932cd0d1255
Scott Weinstein <sweinstein27@gmail.com>
2018-06-28 19:12:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,5 +43,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT category, SUM(pledges.amount) FROM projects"
+"SELECT category, SUM(pledges.amount) FROM projects
+JOIN pledges on pledges.project_id = projects.id
+where category = 'books'"
 end
