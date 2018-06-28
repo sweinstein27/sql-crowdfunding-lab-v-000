35068eeab5bcc1f87d0a2c3778f7b2872a2aca25
Scott Weinstein <sweinstein27@gmail.com>
2018-06-28 19:12:27 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,14 +31,19 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT name, SUM(pledges.amount) FROM users
 JOIN pledges on users.id = pledges.user_id
-GROUP BY name
-ORDER BY pledges.amount"
+GROUP BY users.name
+ORDER BY SUM(pledges.amount), users.name"
 end
 
-# def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-# "Write your SQL query Here"
-# end
-#
-# def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-# "Write your SQL query Here"
-# end
+
+def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
+"SELECT projects.category, pledges.amount FROM projects
+ JOIN pledges on pledges.project_id = projects.id
+ WHERE category = 'music'"
+end
+
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
+"SELECT category, SUM(pledges.amount) FROM projects
+JOIN pledges on pledges.project_id = projects.id
+where category = 'books'"
+end
