f4f9d2cb6e6fc3b9331e0dc327152551564566bf
Scott Weinstein <sweinstein27@gmail.com>
2018-06-28 15:46:20 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,34 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT title, amount FROM projects
- INNER JOIN pledges
+"SELECT title, SUM(amount) FROM projects
+ JOIN pledges
  ON pledges.project_id = projects.id
- ORDER BY users.name;"
+ GROUP BY title
+ ORDER BY title;"
 end
 
-# def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-# "SELECT name, age, amount FROM users
-# JOIN pledges ON users.id = pledges.user_id"
-# end
-#
-# def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-# "Write your SQL query Here"
-# end
-#
-# def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-# "Write your SQL query Here"
-# end
-#
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
+"SELECT name, age, SUM(amount) FROM users
+JOIN pledges ON users.id = pledges.user_id
+GROUP BY name
+ORDER BY name"
+end
+
+def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+"SELECT title, SUM(pledges.amount)-funding_goal FROM projects
+JOIN pledges ON projects.id = pledges.project_id
+GROUP BY title
+HAVING SUM(pledges.amount) >= funding_goal"
+end
+
+def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
+"SELECT name, SUM(pledges.amount) FROM users
+JOIN pledges on users.id = pledges.user_id
+GROUP BY name
+ORDER BY pledges.amount"
+end
+
 # def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 # "Write your SQL query Here"
 # end
