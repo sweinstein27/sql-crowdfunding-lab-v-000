28494e375357543b374413ef43fbdc9ca24567d3
Scott Weinstein <sweinstein27@gmail.com>
2018-06-28 19:10:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount FROM projects
- JOIN pledges on pledges.project_id = project.id
+ JOIN pledges on pledges.project_id = projects.id
  WHERE category = 'music'"
 end
 
